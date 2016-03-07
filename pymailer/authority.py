9409c471455ce24ef991ea8e2ac755dd4e20f1ad
Jieyi Wu <wu.jieyi@brilliantservice.co.jp>
2016-03-07 15:23:35 +0900

    1. Change file name.

---
+++ pymailer/authority.py
@@ -0,0 +1,66 @@
+""" Created by Jieyi on 2/5/16. """
+import smtplib
+
+
+# Decorator.
+class Authority:
+    """
+    Authority decorator. Including login and logout's fixed actions.
+    """
+
+    def __init__(self, uid, pwd, mail_service_ip):
+        self.__id = uid
+        self.__pwd = pwd
+        self.__mail_service_ip = mail_service_ip
+
+    def __call__(self, f):
+        def wrapper(*args, **kwargs):
+            # '*args' parameters are gotten by caller.
+            res = None
+
+            # If login is successful, get the service object.
+            print('Try to login to your mail server...')
+            self.__s = self._login()
+            if not self.__s:
+                print('** Login was failed!!! :(\n')
+                return res
+
+            print('Login was successful!!\n')
+            # Here parameters will send to the decoratee. 'args[0]' is decoratee itself.
+            res = f(args[0], args[1], self.__s)
+
+            # After operation action, auto logout.
+            print('Logout your mail server...')
+            self._logout()
+            print('Logout!!\n')
+            return res
+
+        return wrapper
+
+    # Login to server.
+    def _login(self):
+        try:
+            self.__service = smtplib.SMTP(self.__mail_service_ip, 587)
+            self.__service.ehlo()
+            self.__service.starttls()
+            self.__service.ehlo()
+            self.__service.login(self.__id, self.__pwd)
+        except Exception as e:
+            print(str(e))
+            return None
+
+        return self.__service
+
+    # Logout from server.
+    def _logout(self):
+        self.__service.quit()
+        pass
+
+
+# Testing code
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
---
+++ pymailer/mail.py
@@ -0,0 +1,111 @@
+""" Created by Jieyi on 2/5/16. """
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+
+class Mail:
+    """
+    Mail format. All of the mail information in this class.
+    """
+
+    class MailBuilder:
+        """
+        Mail builder inner.
+        """
+
+        def __init__(self):
+            self.__id = None
+            self.__mailTo = None
+            self.__mailSub = None
+            self.__content = None
+            self.__attach = []
+
+        def uid(self, uid):
+            self.__id = uid
+            return self
+
+        def to(self, to):
+            self.__mailTo = to
+            return self
+
+        def subject(self, subject):
+            self.__mailSub = subject
+            return self
+
+        def content(self, content):
+            self.__content = content
+            return self
+
+        def attach(self, attachment):
+            self.__attach.append(str(attachment))
+            return self
+
+        def build(self):
+            return Mail(self.__id, self.__mailTo, self.__mailSub, self.__content, self.__attach)
+
+    def __init__(self, uid, to, subject, content, attach):
+        self.__msg = MIMEMultipart()
+        self.__id = uid
+        self.__mailTo = to
+        self.__mailSub = subject
+        self.__content = content
+        self.__attachment = attach
+
+    # Create a mail.
+    def create_mail(self):
+        """
+        Create a mail content, including sender, receiver, subject, and attachment......
+
+        :return: All of the mail content.
+        """
+
+        # Mail header
+        self.__msg['From'] = self.__id
+        self.__msg['To'] = self.__mailTo
+        self.__msg['Subject'] = self.__mailSub
+
+        # Mail content
+        self.__msg.attach(MIMEText(self.__content))
+
+        # Mail attachment
+        res, error_msg = self._create_attachment()
+        if not res:
+            raise FileNotFoundError(error_msg)
+
+        return self.__msg
+
+    # Create attachments.
+    # Users don't have too call this method. Create a mail has already included this.
+    def _create_attachment(self):
+        """
+        Attach the necessary file to customer.
+
+        :return: result, True: Success, False: Failed. lack_file_name, Which file you didn't attach.
+        """
+
+        res = True
+        lack_file_name = ''
+        for path in self.__attachment:
+            try:
+                # Get the file name from file path.
+                self.__filename = path.split('/')[-1]
+
+                # Attach the file to mail information.
+                self.att = MIMEText(open(path, 'rb').read(), 'base64', 'gb2312')
+                self.att['Content-Type'] = 'application/octet-stream'
+                self.att['Content-Disposition'] = 'attachment; filename="%s"' % self.__filename
+
+                self.__msg.attach(self.att)
+            except FileNotFoundError as fnfe:
+                res = True
+                lack_file_name += str(fnfe) + '\n'
+        return res, lack_file_name
+
+
+# Testing code
+def main():
+    print()
+
+
+if __name__ == '__main__':
+    main()
