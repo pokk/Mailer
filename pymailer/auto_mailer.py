3f6e5adcdf0a98f5fc96169e3628429b8aa0edbc
Jieyi <pokkbaby@gmail.com>
2016-03-09 00:16:10 +0900

    1. Add the progress bar.

--- pymailer/auto_mailer.py
+++ pymailer/auto_mailer.py
@@ -1,8 +1,10 @@
 """ Created by Jieyi on 2/4/16. """
 import threading
+import tkinter
 import tkinter.messagebox as messagebox
 from tkinter import Frame, Label, Entry, Button, END
 from tkinter.scrolledtext import ScrolledText
+from tkinter.ttk import Progressbar
 
 from internet import InternetStatus
 from mailer import Mailer
@@ -15,6 +17,7 @@ class Application(Frame):
 
         Frame.__init__(self, master)
         self.grid()
+        self.master = master
         # UI components.
         self.receiver_email_text = Label(self, text="Receiver:")
         self.receiver_email_field = Entry(self, width=50)
@@ -22,7 +25,9 @@ class Application(Frame):
         self.subject_field = Entry(self, width=50)
         self.receiver_name_text = Label(self, text='Name:')
         self.receiver_name_field = Entry(self, width=50)
+        self.send_progressbar = Progressbar(self, orient='horizontal', length=500, mode='determinate', maximum=300)
         self.send_button = Button(self, text='Send', command=self.__send_mail)
+        self.quit_button = Button(self, text='Exit', command=self.__exit)
         self.log_msg_text = ScrolledText(self)
         self.__create_widgets()
 
@@ -41,12 +46,16 @@ class Application(Frame):
         self.subject_field.insert(0, 'Osaka Mariko Apartment Location & Condition')
         self.receiver_name_text.grid(row=2, column=0)
         self.receiver_name_field.grid(row=2, column=1, columnspan=6)
-        self.send_button.grid(row=3, column=3)
-        self.log_msg_text.grid(row=4, column=0, columnspan=7)
+        self.send_progressbar.grid(row=3, column=0, columnspan=7)
+        self.send_button.grid(row=4, column=2)
+        self.quit_button.grid(row=4, column=3)
+        self.log_msg_text.grid(row=5, column=0, columnspan=7)
 
     def __send_mail(self):
         def inner_send_mail():
             self.lock = True  # Lock this process.
+            self.send_progressbar['value'] = 0
+            self.send_progressbar.start()
             # Clear the log text area.
             self.log_msg_text.delete('1.0', END)
             # Error checking.
@@ -57,6 +66,7 @@ class Application(Frame):
 
             ending = 'Welcome to use my application :)' if Mailer().send_mail() else '** Your sending was failed :( please send it again!'
             self.log_msg_text.insert(END, ending)
+            self.send_progressbar.stop()
             self.lock = False  # Unlock this process.
 
         if not self.lock:
@@ -80,6 +90,13 @@ class Application(Frame):
         self.log_msg_text.insert(END, 'Internet is ok!\n\n')
         return True
 
+    def __exit(self):
+        if not self.lock:
+            self.log_msg_text.insert(END, '\n\n -- Bye Bye --\n')
+            self.master.quit()
+        else:
+            messagebox.showinfo('Error', "Now it's processing...please wait it ;)")
+
 
 # Main function for all library.
 def main():
@@ -88,7 +105,7 @@ def main():
       add os.path.abspath(os.pardir). Because of the shell script, we need
       to add correct file path.
     """
-    app = Application()
+    app = Application(tkinter.Tk())
     app.master.title('Auto Mailer')
     app.mainloop()
 
